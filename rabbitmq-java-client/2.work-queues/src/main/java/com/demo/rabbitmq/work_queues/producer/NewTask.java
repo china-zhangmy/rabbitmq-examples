2f5927e2d6a3b4b7e7cb27540608f052cc973731
china-zhangmy <china.zhangmy@hotmail.com>
2018-06-26 19:08:22 +0800

    RabbitMQ Examples

---
+++ rabbitmq-java-client/2.work-queues/src/main/java/com/demo/rabbitmq/work_queues/producer/NewTask.java
@@ -0,0 +1,51 @@
+package com.demo.rabbitmq.work_queues.producer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.util.Date;
+
+public class NewTask {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        boolean durable = true; // declare queue durable
+        channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
+        String message = getMessages(args);
+        channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes("UTF-8"));
+        System.out.println(" [X] sent  '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+
+    private static String getMessages(String[] strings) {
+        if (strings.length < 1) {
+            return "Hello World .......... " + new Date().getTime();
+        }
+
+        return joinStrings(strings, " ");
+    }
+
+    private static String joinStrings(String[] strings, String delimiter) {
+        int length = strings.length;
+        if (length == 0) {
+            return "";
+        }
+
+        StringBuffer words = new StringBuffer(strings[0]);
+        for (int i = 1; i < length; i++) {
+            words.append(delimiter).append(strings[i]);
+        }
+
+        return words.toString();
+    }
+}
---
+++ rabbitmq-java-client/3.publish-subscribe/src/main/java/com/demo/rabbitmq/publish_subscribe/producer/EmitLog.java
@@ -0,0 +1,50 @@
+package com.demo.rabbitmq.publish_subscribe.producer;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.Date;
+
+public class EmitLog {
+
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+        String message = getMessages(args);
+        channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+        System.out.println(" [X] sent  '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+
+    private static String getMessages(String[] strings) {
+        if (strings.length < 1) {
+            return "info: Hello World " + new Date().getTime();
+        }
+
+        return joinStrings(strings, " ");
+    }
+
+    private static String joinStrings(String[] strings, String delimiter) {
+        int length = strings.length;
+        if (length == 0) {
+            return "";
+        }
+
+        StringBuffer words = new StringBuffer(strings[0]);
+        for (int i = 1; i < length; i++) {
+            words.append(delimiter).append(strings[i]);
+        }
+
+        return words.toString();
+    }
+}
